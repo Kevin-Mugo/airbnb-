23473fbfcf2fae0c81cd1476cf7d02d6d44a9b2f
ANZY KENYA <mugokevin900@gmail.com>
2022-12-31 12:21:48 +0300

    #2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 import data from './data';
+import App from './App';
 
 const cards = data.map(item => {
   return(
@@ -21,6 +22,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
+    
   <Navbar/>
    <Hero/>
    <section className="cards-list">
